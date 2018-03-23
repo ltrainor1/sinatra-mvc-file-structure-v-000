3a1377eabeb5066e4a73060756c2018ea91f210e
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-23 22:02:11 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,12 @@
+class Dog  
+
+  attr_accessor :name, :breed, :age 
+  @@all = []
+  
+def initialize(name = nil, breed = nil, age = nil)
+  @name = name 
+  @age = age
+  @breed = breed 
+  @@all << self
+end 
+end 
